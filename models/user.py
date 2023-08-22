82bd49003fe87014f61de726096682486bac5b1c
injili <nyarekigospel@gmail.com>
2023-08-22 07:11:57 -0700

    user models

--- models/user.py
+++ models/user.py
@@ -7,7 +7,11 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
+    id = Column(String(60), primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
