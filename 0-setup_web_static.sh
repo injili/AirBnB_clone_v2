f4916da21897d6dbac0e413d0a065e7a3bd7c65d
injili <nyarekigospel@gmail.com>
2023-09-08 04:26:52 +0000

    prepare web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,25 +6,27 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 
 # create folders
-[ ! -d "/data" ] && mkdir /data
-[ ! -d "/data/web_static" ] && mkdir /data/web_static
-[ ! -d "/data/web_static/releases" ] && mkdir /data/web_static/releases
-[ ! -d "/data/web_static/releases/test" ] && mkdir /data/web_static/releases/test
-
-[ ! -d "/data/web_static/shared" ] && mkdir /data/web_static/shared
+sudo mkdir -P /data/web_static/releases/test /data/web_static/shared
 
 # create the test html file
 sudo sh -c "echo '
-<!DOCTYPE HTML>
-<html>
-        <head>
-		<meta charset='UTF-8'>
-                <title>FakeHTMLFile</title>
-        </head>
-        <body>
-                <p>The difference between a saint and a sinner is a good reason</p>
-        </body>
-</html>
+The difference between a saint and a sinner is a good reason
 ' >> /data/web_static/releases/test/index.html"
 
-#create symbolic link
+# create symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# giving ownership of the /data/ folder to ubuntu user and group
+sudo chown -hR ubuntu:ubuntu /data
+
+# updating the Nginx configuration to serve the content to hbnb_static
+STATIC_WEB="\\
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+	}
+"
+
+sudo sed -i "35i $STATIC_WEB" /etc/nginx/sites_available/default
+
+# restart Nginx
+sudo service nginx restart
