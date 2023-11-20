6cf874f7da7175200ddcdf2241d3e40999d77bc7
injili <nyarekigospel@gmail.com>
2023-11-20 04:19:57 -0800

    C is fun. Or is it?

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Display custom text and C, C is fun
+"""
+from flask import Flask
+
+if __name__ = ('__main__')
+
+
+@app.route('/')
+def hello():
+    """
+    Displays Hello HBNB
+    """
+    return ("Hello HBNB")
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Displays HBNB when called
+    """
+    return ("HBNB")
+
+
+@app.route('/c/<text>')
+def c_ic_fun(text):
+    """
+    Displays C followed by the value in 'text'
+    """
+    response = 'C {}'.format(text)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', strict_slashes=False)
