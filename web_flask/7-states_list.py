7ab46bc539389f67678bd9bdcdf7e81327a07ecc
injili <nyarekigospel@gmail.com>
2023-11-20 07:06:19 -0800

    list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,11 +5,12 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', script_slashes=False)
-def states ():
+def states_list():
     """Render html page to display new created States"""
     states = storage.all()
     return render_template('7-states_list.html', states=states)
@@ -22,4 +23,4 @@ def teardown_session(self):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=None)
