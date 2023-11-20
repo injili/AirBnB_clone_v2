e2c9aa093be50bb0098071eb6c1b1c3e0fd77e30
injili <nyarekigospel@gmail.com>
2023-11-20 05:02:00 -0800

    python is cool, aint it?

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,11 +25,13 @@ def c_is_fun():
     return response
 
 
-@app.route('python/', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
-    """Returns "Python "followed by the text passed
-    If no text is passedit returns Python is cool"""
+    """
+    Returns "Python" followed by the text passed
+    If no text is passedit returns Python is cool
+    """
     response = "Python {}".format(text.replace("_", " "))
     return response
 
